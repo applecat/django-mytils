edb6b2af687052f2107e850d934d6dc54c19c35e
Dmitry <d.akinin@gmail.com>
2020-05-20 00:39:31 +0300

    0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Dynamically calculate the version based on mytils.VERSION.
-version = '0.0.4'
+version = '0.0.5'
 
 setup(
     name='django-mytils',
